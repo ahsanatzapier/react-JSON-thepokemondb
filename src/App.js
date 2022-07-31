a36f8132769ca2be0d3aa204331a5558366a76f0
ahsanatzapier <ahsan.hassan@zapier.com>
2022-07-31 12:56:47 -0600

    Implemented search field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { mockComponent } from 'react-dom/test-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      pokemons: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/pokedex.json')
+      .then(response => response.json())
+      .then(pokemons => this.setState(() => {
+        return { pokemons }
+      }))
+  }
+
+
+  render() {
+
+    const filteredPokemons = this.state.pokemons.filter((pokemon) => {
+      return pokemon.name.english.toLocaleLowerCase().includes(this.state.searchField)
+
+    })
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search Character'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase()
+            this.setState(() => {
+              return { searchField }
+            })
+          }
+          }
+        />
+        
+        {filteredPokemons.map((pokemon) => {
+          return (
+            <div key={pokemon.id}>
+              <h1>{pokemon.name.english}</h1>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
