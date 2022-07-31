62544d70ece7bcd7e83f05e218d397ace10ee580
ahsanatzapier <ahsan.hassan@zapier.com>
2022-07-31 16:18:08 -0600

    Implemented Styling

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,25 @@
 import { Component } from "react";
+import './card-list.styles.css'
 
 class CardList extends Component {
+ 
 
   render() {
 
     const { pokemons } = this.props
 
     return (
-         <div>
-      {pokemons.map(pokemon => (
-          <h1 key={pokemon.id}>{pokemon.name.english}</h1>
-        ))}
-        </div>
+      <div className='card-list'>
+        {pokemons.map((pokemon) => {
+          const {english} = pokemon.name
+          const {hires} = pokemon.image
+          return (
+            <div key={pokemon.id} className='card-container'>
+              <img className='card-image' alt={`pokemon ${english}`} src={hires} width="150" height="150"/>
+              <h2>{english}</h2>
+            </div>
+          )})}
+      </div>
     )
   }
 }
