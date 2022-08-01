314c0a42a40e38e96f2de644a2115502a4fbd9bf
ahsanatzapier <ahsan.hassan@zapier.com>
2022-08-01 05:36:41 -0600

    Change 2 columns on mobile to 3

--- src/components/card-list.jsx
+++ src/components/card-list.jsx
@@ -2,7 +2,7 @@ import CardContainer from "./card-container";
 
 
 const CardList = ({ pokemons }) => (
-<div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+<div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
     {pokemons.map((pokemon) => {
       const { english } = pokemon.name
       const { id } = pokemon
