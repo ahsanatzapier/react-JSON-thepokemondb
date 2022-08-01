c79d6647bbdb640b5cac9767975f3ce4556156f9
ahsanatzapier <ahsan.hassan@zapier.com>
2022-07-31 23:54:14 -0600

    Refactored App to use functional components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,52 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import { Component } from 'react';
+
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
+const App = () => {
 
-  constructor() {
-    super();
+  // hooks
+  const [searchField, setSearchField] = useState('')
+  const [pokemons, setPokemons] = useState([])
+  const [filteredPokemons, setFilteredPokemon] = useState(pokemons)
 
-    this.state = {
-      pokemons: [],
-      searchField: ''
-    }
-  }
-
-  componentDidMount() {
+  // setting up pokemons using side effect
+  useEffect(() => {
     fetch('https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/pokedex.json')
       .then(response => response.json())
-      .then(pokemons => this.setState(() => {
-        return { pokemons }
-      }))
-  }
+      .then(pokemons => setPokemons(pokemons))
+  }, [])
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase()
-    this.setState(() => {
-      return { searchField }
-    })
-  }
-
-  render() {
-
-    const { pokemons, searchField } = this.state
-    const { onSearchChange } = this
-
-    const filteredPokemons = pokemons.filter((pokemon) => {
+  // only render again if pokemons array changes or the search field changes
+  useEffect(() => {
+    const newFilteredPokemons = pokemons.filter((pokemon) => {
       return pokemon.name.english.toLocaleLowerCase().includes(searchField)
     })
+    setFilteredPokemon(newFilteredPokemons)
+  }, [pokemons, searchField])
 
-    return (
-      <div className='App'>
-        <p><img src="https://fontmeme.com/permalink/220801/cd6adc6038b8d745ebf5e778582bbcf4.png" alt="pokemon-font" width="600" height="104"/></p>
-        <SearchBox
-          className='pokemon-search-box'
-          placeholder='Search Pokemon'
-          onSearchHandler={onSearchChange} />
-        <CardList pokemons={filteredPokemons} />
-      </div>
-    )
+  // functions
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
   }
+
+  return (
+
+    <div className='App'>
+      <h1>React PokeDex</h1>
+      
+      <SearchBox
+        className='pokemon-search-box'
+        placeholder='Search Pokemon'
+        onSearchHandler={onSearchChange} />
+        
+      <CardList pokemons={filteredPokemons} />
+    </div>
+
+  )
 }
+
 
 export default App;
